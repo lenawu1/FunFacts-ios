c8feb8d9084f6e7e427ac0e23e2b6ea49742d6bf
lenawu1 <lena.m.wu@gmail.com>
2024-03-21 11:33:00 -0700

    Initial Commit

---
+++ facts!/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  facts!
+//
+//  Created by Lena Wu on 3/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ facts!/facts_App.swift
@@ -0,0 +1,17 @@
+//
+//  facts_App.swift
+//  facts!
+//
+//  Created by Lena Wu on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct facts_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
